e5b3b28cd70955273c612104dfdac159e7e17a32
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2024-10-20 22:00:37 +0600

    db connected

--- server/index.js
+++ server/index.js
@@ -1,12 +1,41 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Server up and running!');
+const uri = process.env.MONGODB_URI;
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+        // Send a ping to confirm a successful connection
+        const db = await client.db("animal-kingdom").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        const animalCollection = client.db("animal-kingdom").collection("animals");
+
+
+
+
+    } catch (error) {
+        console.log(error);
+    } finally {
+
+        await client.close();
+    }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
