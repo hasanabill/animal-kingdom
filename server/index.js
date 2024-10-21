310039a2b721a4fed1dc88fac3d8cef565937df8
Mahmudul Hasan Anabil <hasanabilna@gmail.com>
2024-10-21 23:55:01 +0600

    object id error fixed

--- server/index.js
+++ server/index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
